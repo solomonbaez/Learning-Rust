a50761d53690f246bf490956063e6ff137946a65
Solomon Baez <solomon@Solomons-Air.huawei.net>
2023-04-21 17:07:08 -0600

    Working through the Rust-Lang book

---
+++ closures/target/debug/deps/closures-a424c9363a236c33.d
@@ -0,0 +1,5 @@
+/Users/solomon/rust-projects/closures/target/debug/deps/closures-a424c9363a236c33.rmeta: src/main.rs
+
+/Users/solomon/rust-projects/closures/target/debug/deps/closures-a424c9363a236c33.d: src/main.rs
+
+src/main.rs:
