d34e4a6be0f05a6e51d7a7f26a503ad1d65b2653
solomonbaez <solomon.james.baez@gmail.com>
2023-12-02 01:28:53 -0800

    Built out a sweet linear congruential generator for a basic guessing game

--- guessing_game_3/src/main.rs
+++ guessing_game_3/src/main.rs
@@ -1,7 +1,25 @@
 use::std::io;
+use::std::time::{SystemTime, UNIX_EPOCH};
+
+//linear congruential generator
+#[allow(non_snake_case)]
+fn gen_LCG_rand() -> u32 {
+    let now = SystemTime::now();
+    // UNIX_EPOCH anchors systime
+    let since_epoch = now.duration_since(UNIX_EPOCH).
+        expect("failed to anchor epoch");
+
+    let seed = since_epoch.as_secs();
+    let a: u32 = 1103515245;
+    let c: u32 = 12345;
+    let m = 2_i64.pow(31) - 1;
+
+    return 
+        (a.wrapping_mul(seed as u32).wrapping_add(c)) % m as u32;
+}
 
 fn main() {
-    let seed = String::from("22");
+    let rand = gen_LCG_rand();
 
     println!("Welcome to GG3.0");
     println!("Please insert your first guess:");
@@ -13,12 +31,14 @@ fn main() {
         .read_line(&mut guess)
         .expect("failed to read guess!");
 
-    let trimmed = guess.trim();
-    println!("Your guess: {}", trimmed);
-    if seed == trimmed {
+    let parsed_guess: u32 = guess.trim().parse().unwrap();
+    println!("Your guess: {}", parsed_guess);
+
+    if rand == parsed_guess {
         println!("Nice! You guessed correct :)");
     } else {
         println!("Sorry, try again!");
     }
-    println!("The correct answer is: {}", seed);
+
+    println!("The correct answer is: {}", rand);
 }
