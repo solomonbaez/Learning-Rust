197acc93c4610f313e4e5cd213e40335243e9f82
solomonbaez <solomon.james.baez@gmail.com>
2023-12-03 23:30:36 -0800

    Adjusting my_filter for generic closures

--- functional_programming/src/main.rs
+++ functional_programming/src/main.rs
@@ -1,23 +1,17 @@
 fn main() {
     let x = Vec::<u32>::from([1, 2, 4]);
-    let filtered_x = my_filter(x, &filter_is_even);
-    println!("{}, {}", filtered_x[0], filtered_x[1])
+    let is_even = my_filter(&x, &|x| x % 2 == 0);
+    let is_odd = my_filter(&x, &|x| x % 2 != 0);
+    println!("{}, {}", is_even[0], is_odd[0])
 }
 
-fn my_filter<T: Copy>(input: Vec<T>, filter_fn: &dyn Fn(T) -> bool) -> Vec<T> {
+fn my_filter<T: Copy>(input: &Vec<T>, filter_fn: &dyn Fn(T) -> bool) -> Vec<T> {
     let mut filtered_vec = Vec::<T>::new();
     input.into_iter().for_each(|v| {
-        if filter_fn(v) {
-            filtered_vec.push(v)
+        if filter_fn(*v) {
+            filtered_vec.push(*v)
         }
     });
 
     filtered_vec
 }
-
-fn filter_is_even(x: u32) -> bool {
-    (x % 2) == 0
-}
-// fn filter_is_odd(x: u32) -> bool {
-//     (x % 2) != 0
-// }
