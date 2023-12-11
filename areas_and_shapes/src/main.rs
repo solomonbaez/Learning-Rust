c7dfd7ef973d22e0a1a0167ed0dc3d151351c155
solomonbaez <solomon.james.baez@gmail.com>
2023-12-11 00:21:47 -0800

    Added rectangle variant to area calculator

--- areas_and_shapes/src/main.rs
+++ areas_and_shapes/src/main.rs
@@ -2,6 +2,7 @@ use ::std::f32::consts::PI;
 
 enum Shape {
     Circle { r: f32 },
+    Rectangle { l: f32, w: f32, h: f32 },
 }
 impl Shape {
     fn area(&self) -> f32 {
@@ -9,6 +10,7 @@ impl Shape {
             Shape::Circle { r } => {
                 return PI * r.powf(2.0);
             }
+            Shape::Rectangle { l, w, h } => return l * w * h,
         };
     }
 }
@@ -17,4 +19,12 @@ fn main() {
     let circle = Shape::Circle { r: 10.0 };
     let circle_area = circle.area();
     println!("{}", circle_area);
+
+    let rectangle = Shape::Rectangle {
+        l: 1.0,
+        w: 2.0,
+        h: 2.0,
+    };
+    let rectangle_area = rectangle.area();
+    println!("{}", rectangle_area);
 }
